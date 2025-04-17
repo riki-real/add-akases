8e9685ab27d749b0b9881fdaf1583edd9116e63a
riki-real <jbakunriki@gmail.com>
2025-04-17 09:12:58 +0700

    Delete api/getdata.js

--- api/getdata.js
+++
@@ -1,7 +0,0 @@
-export default async function handler(req, res) {
-  if (req.method !== "GET") {
-    return res.status(405).json({ message: "Method Not Allowed" });
-  }
-
-  // logic ambil data dari GitHub
-}
